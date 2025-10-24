847db2bc7dacde2b31874dfdb8990f52e1e41477
Martin Oywa <martinoywa2@gmail.com>
2025-10-24 09:52:59 +0300

    Add first_non_repeating_char function
    Implement a function to find the first non-repeating character in a string.

---
+++ first_non_repeating_char_ht.py
@@ -0,0 +1,30 @@
+def first_non_repeating_char(string):
+    # generate a dictonary of character counts
+    # then iterate through the counts checking for count==1
+    counts = {}
+    for c in string:
+        counts[c] = counts.get(c, 0) + 1
+        
+    for k, v in counts.items(): # order will alway be guaranteed in Python 3.7+
+        if v == 1:
+            return k
+    return None
+
+
+
+print( first_non_repeating_char('leetcode') )
+
+print( first_non_repeating_char('hello') )
+
+print( first_non_repeating_char('aabbcc') )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    l
+    h
+    None
+
+"""
